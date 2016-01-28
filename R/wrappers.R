02d3971d13d3fc56be5941f1007d1f0f7bb35d00
Aitor Gonzalez <aitor@pedagogix>
2016-01-28 23:06:47 +0100

    ENH Scores returns probs

--- R/wrappers.R
+++ R/wrappers.R
@@ -27,10 +27,10 @@ scoreData <- function(x, ledpred=NULL, model=NULL, score.file = NULL) {
 scores=NULL
 
 if(!is.null(ledpred))  {
-	scores = ledpred$model.obj$ScoreData(x=x)$scores
+	scores = ledpred$model.obj$ScoreData(x=x)$probs
 
 } else if (!is.null(model))  {
-	scores = model$ScoreData(x=x)$scores
+	scores = model$ScoreData(x=x)$probs
 }
 
 if(!is.null(score.file)) {
