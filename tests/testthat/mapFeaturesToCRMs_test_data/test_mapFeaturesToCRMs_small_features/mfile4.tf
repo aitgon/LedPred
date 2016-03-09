83ff5971c7b573059d8c33eb443cfba5f9edd840
Aitor Gonzalez <aitor@pedagogix>
2016-03-09 21:54:29 +0100

    ENH Added mapFeaturesToCRMs function

---
+++ tests/testthat/mapFeaturesToCRMs_test_data/test_mapFeaturesToCRMs_small_features/mfile4.tf
@@ -0,0 +1,154 @@
+AC dTCF
+XX
+ID  dTCF
+XX
+DE  sgCCTTTGATttt
+PO       A     C     G     T
+1       12    18    24    11
+2       15    13    24    13
+3        6    57     2     0
+4        0    65     0     0
+5        0     0     0    65
+6        0     2     1    62
+7        0    11     0    54
+8        0     2    58     5
+9       51     0     0    14
+10       3     3     0    59
+11       6    11     9    39
+12      15    16    14    20
+13      11    15    10    29
+XX
+CC  program: transfac
+CC  matrix.nb: 2
+CC  accession: oligos_6nt_mkv3_m2
+CC  AC: oligos_6nt_mkv3_m2
+CC  id: oligos_6nt_mkv3_m2
+CC  version: 
+CC  name: oligos_6nt_mkv3_m2
+CC  description: sgCCTTTGATttt
+CC  transfac_consensus: 
+CC  matrix.nb: 2
+CC  bg_markov_order: 1
+CC  consensus.strict: ggCCTTTGATttt
+CC  consensus.strict.rc: AAAATCAAAGGCC
+CC  consensus.IUPAC: sgCCTTTGATtyy
+CC  consensus.IUPAC.rc: RRAATCAAAGGCS
+CC  consensus.regexp: [cg]gCCTTTGATt[ct][ct]
+CC  consensus.regexp.rc: [AG][AG]AATCAAAGGC[CG]
+XX
+//
+
+AC tin
+XX
+ID  tin
+XX
+DE  msTCAAGTGgcyr
+PO       A     C     G     T
+1       15    18    12    12
+2        5    21    19    12
+3        8     6     3    40
+4        0    57     0     0
+5       54     1     1     1
+6       56     0     0     1
+7        0     0    57     0
+8        0     0     0    57
+9        0     0    57     0
+10       3    12    38     4
+11      10    28     9    10
+12      13    15     9    20
+13      17    11    15    14
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: oligos_6nt_mkv2_m1
+CC  AC: oligos_6nt_mkv2_m1
+CC  id: oligos_6nt_mkv2_m1
+CC  version: 
+CC  name: oligos_6nt_mkv2_m1
+CC  description: msTCAAGTGgcyr
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  bg_markov_order: 1
+CC  consensus.strict: cctCAAGTGGctg
+CC  consensus.strict.rc: CAGCCACTTGAGG
+CC  consensus.IUPAC: cstCAAGTGGcyr
+CC  consensus.IUPAC.rc: YRGCCACTTGASG
+CC  consensus.regexp: c[cg]tCAAGTGGc[ct][ag]
+CC  consensus.regexp.rc: [CT][AG]GCCACTTGA[CG]G
+XX
+//
+
+AC doc
+XX
+ID  doc
+XX
+DE  arCAGRTAGhs
+PO       A     C     G     T
+1       73    48    46    46
+2       54    45    73    41
+3        0   213     0     0
+4      213     0     0     0
+5        0     0   213     0
+6       58     0   155     0
+7        0     0     0   213
+8      213     0     0     0
+9        0     0   213     0
+10      61    61    34    57
+11      49    60    59    45
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: oligos_6nt_mkv3_m1
+CC  AC: oligos_6nt_mkv3_m1
+CC  id: oligos_6nt_mkv3_m1
+CC  version: 
+CC  name: oligos_6nt_mkv3_m1
+CC  description: arCAGRTAGhs
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  bg_markov_order: 1
+CC  consensus.strict: agCAGGTAGcg
+CC  consensus.strict.rc: CGCTACCTGCT
+CC  consensus.IUPAC: agCAGRTAGms
+CC  consensus.IUPAC.rc: SKCTAYCTGCT
+CC  consensus.regexp: agCAG[AG]TAG[ac][cg]
+CC  consensus.regexp.rc: [CG][GT]CTA[CT]CTGCT
+XX
+//
+
+AC pnr
+XX
+ID  pnr
+XX
+DE  amAGATACad
+PO       A     C     G     T
+1     1145   440   542   657
+2      713  1070   479   522
+3     2784     0     0     0
+4        0     0  2784     0
+5     2784     0     0     0
+6        0     0     0  2784
+7     2784     0     0     0
+8        0  2784     0     0
+9     1261   387   495   641
+10     740   590   758   696
+XX
+CC  program: transfac
+CC  matrix.nb: 3
+CC  accession: oligos_6nt_mkv4_m3
+CC  AC: oligos_6nt_mkv4_m3
+CC  id: oligos_6nt_mkv4_m3
+CC  version: 
+CC  name: oligos_6nt_mkv4_m3
+CC  description: amAGATACad
+CC  transfac_consensus: 
+CC  matrix.nb: 3
+CC  bg_markov_order: 1
+CC  consensus.strict: acAGATACag
+CC  consensus.strict.rc: CTGTATCTGT
+CC  consensus.IUPAC: acAGATACas
+CC  consensus.IUPAC.rc: STGTATCTGT
+CC  consensus.regexp: acAGATACa[cg]
+CC  consensus.regexp.rc: [CG]TGTATCTGT
+XX
+//
