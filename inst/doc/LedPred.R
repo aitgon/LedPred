e63270450e223a89ba05d0accbc4ef94daee3844
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2016-03-24 19:11:23 +0000

    1.3.5
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/LedPred@115242 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/doc/LedPred.R
+++ inst/doc/LedPred.R
@@ -47,11 +47,11 @@ mcols(crm.features)=vals_mod
 ###################################################
 cost.vector <- c(1,3,10,30)
 gamma.vector <- c(1,3,10,30)
-c.g.obj <- mcTune(data.granges= crm.features, ranges = list(cost=cost.vector, 
-    gamma=gamma.vector), kernel='linear', file.prefix = "test")
-names(c.g.obj)
- cost <- c.g.obj$best.parameters$cost
- gamma <- c.g.obj$best.parameters$gamma
+#c.g.obj <- mcTune(data.granges= crm.features, ranges = list(cost=cost.vector, 
+#    gamma=gamma.vector), kernel='linear', file.prefix = "test")
+#names(c.g.obj)
+# cost <- c.g.obj$best.parameters$cost
+# gamma <- c.g.obj$best.parameters$gamma
 
 
 ###################################################
@@ -64,33 +64,33 @@ names(c.g.obj)
 ###################################################
 ### code chunk number 6: LedPred.Rnw:194-198 (eval = FALSE)
 ###################################################
-##  feature.nb.obj <- tuneFeatureNb(data.granges=crm.features, 
-##      feature.ranking=feature.ranking, kernel='linear', cost=cost,gamma=gamma, 
-##      file.prefix = "test")
-##  names(feature.nb.obj)
+## # feature.nb.obj <- tuneFeatureNb(data.granges=crm.features, 
+## #     feature.ranking=feature.ranking, kernel='linear', cost=cost,gamma=gamma, 
+## #     file.prefix = "test")
+## # names(feature.nb.obj)
 
 
 ###################################################
 ### code chunk number 7: LedPred.Rnw:210-213
 ###################################################
 feature.nb <- 50
-svm.model <- createModel(data.granges=crm.features, cost=cost, gamma=gamma, 
-    feature.ranking=feature.ranking, feature.nb=feature.nb)
+#svm.model <- createModel(data.granges=crm.features, cost=cost, gamma=gamma, 
+#    feature.ranking=feature.ranking, feature.nb=feature.nb)
 
 
 ###################################################
 ### code chunk number 8: LedPred.Rnw:218-219
 ###################################################
-feature.weights <- as.data.frame(t(t(svm.model$coefs) %*% svm.model$SV))
+#feature.weights <- as.data.frame(t(t(svm.model$coefs) %*% svm.model$SV))
 
 
 ###################################################
 ### code chunk number 9: LedPred.Rnw:225-229
 ###################################################
-probs.labels.list <- evaluateModelPerformance(data.granges=crm.features, 
-    feature.ranking=feature.ranking, feature.nb=50, 
-    file.prefix = "test")
-names(probs.labels.list[[1]])
+#probs.labels.list <- evaluateModelPerformance(data.granges=crm.features, 
+#    feature.ranking=feature.ranking, feature.nb=50, 
+#    file.prefix = "test")
+#names(probs.labels.list[[1]])
 
 
 ###################################################
@@ -105,20 +105,20 @@ ngs.path <- system.file("extdata/ngs", package="LedPred")
 ngs.files=list.files(ngs.path, full.names=TRUE)
 feature.nb=50
 
-prediction.crm.features.list <- mapFeaturesToCRMs(URL = 'http://ifbprod.aitorgonzalezlab.org/map_features_to_crms.php', positive.bed=positive.bed, 
-  pssm=TF.matrices, background.freqs=background.freqs, 
-  genome='dm3', ngs=ngs.files, feature.ranking=feature.ranking, feature.nb=50, 
-    crm.feature.file = "test_pred.crm.features.tab", 
-    stderr.log.file = "test_pred.stderr.log", stdout.log.file = "test_pred.stdout.log")
-names(prediction.crm.features.list)
-prediction.crm.features <- prediction.crm.features.list$crm.features
+#prediction.crm.features.list <- mapFeaturesToCRMs(URL = 'http://ifbprod.aitorgonzalezlab.org/map_features_to_crms.php', #positive.bed=positive.bed, 
+#  pssm=TF.matrices, background.freqs=background.freqs, 
+#  genome='dm3', ngs=ngs.files, feature.ranking=feature.ranking, feature.nb=50, 
+#    crm.feature.file = "test_pred.crm.features.tab", 
+#    stderr.log.file = "test_pred.stderr.log", stdout.log.file = "test_pred.stdout.log")
+#names(prediction.crm.features.list)
+#prediction.crm.features <- prediction.crm.features.list$crm.features
 
 
 ###################################################
 ### code chunk number 11: LedPred.Rnw:263-265
 ###################################################
-pred.test <- scoreData(data.granges=prediction.crm.features, model=svm.model, 
-  score.file="test_prediction.tab")
+#pred.test <- scoreData(data.granges=prediction.crm.features, model=svm.model, 
+#  score.file="test_prediction.tab")
 
 
 ###################################################
